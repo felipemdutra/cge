5c2ef148363ea55c69699127b31a6c46482b3f09
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-17 18:09:47 -0300

    feat: implement renderer in 'Engine' class

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -3,11 +3,12 @@
 #include "engine.hpp"
 
 using namespace cge;
+using std::make_unique;
 
 #define WIDTH 800
 #define HEIGHT 600
 
-Engine::Engine() : window(std::make_unique<Window>()){ }
+Engine::Engine() : window(make_unique<Window>()),  renderer(make_unique<Renderer>()) { }
 
 Engine::~Engine() { }
 
@@ -16,11 +17,19 @@ bool Engine::init() {
         return false;
     }
 
+    if (!renderer->init()) {
+        return false;
+    }
+
     return true;
 }
 
 void Engine::run() {
     while (!window->shouldClose()) {
+        renderer->clear(0.0f, 0.15f, 0.3f, 1.0f);
+
+        renderer->draw();
+
         window->pollEvents();
         window->swapBuffers();
     }
