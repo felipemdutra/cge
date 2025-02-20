49f0a8032d433807d689797f65c9764d2fb42b3c
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-20 19:19:23 -0300

    Refactor 'Engine' implementation: Use dependency injection and remove 'Engine::init()' Update 'Engine' constructor to accept 'Window' and 'Renderer' by reference Remove 'Engine::init()' as sybsystem initialization is now external. Replace pointer access (->) with reference calls (.) in 'run()' Minor tweak to the clear color for consistency.

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,37 +1,24 @@
-#include <memory>
-
 #include "engine.hpp"
+#include "../platform/window.hpp"
+#include "renderer/renderer.hpp"
 
 using namespace cge;
-using std::make_unique;
 
 #define WIDTH 800
 #define HEIGHT 600
 
-Engine::Engine() : window(make_unique<Window>()),  renderer(make_unique<Renderer>()) { }
+Engine::Engine(Window& window, Renderer& renderer) : window(window), renderer(renderer) { }
 
 Engine::~Engine() { }
 
-bool Engine::init() {
-    if (!window->init(WIDTH, HEIGHT, "Game Engine")) {
-        return false;
-    }
-
-    if (!renderer->init()) {
-        return false;
-    }
-
-    return true;
-}
-
 void Engine::run() {
-    while (!window->shouldClose()) {
-        renderer->clear(0.0f, 0.15f, 0.3f, 1.0f);
+    while (!window.shouldClose()) {
+        renderer.clear(0.0f, 0.15f, 0.6f, 1.0f);
 
-        renderer->draw();
+        renderer.draw();
 
-        window->pollEvents();
-        window->swapBuffers();
+        window.pollEvents();
+        window.swapBuffers();
     }
 }
 
