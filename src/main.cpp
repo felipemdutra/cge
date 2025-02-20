727d15ee431126ef2ab93f1233aaf3944066b3a7
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-20 19:21:12 -0300

    refactor main.cpp: Explicit subsystem initialization Move 'Window' and 'Renderer' out of 'Engine'. Add error handling with meaningful message for failures. Update 'Engine' instantiation to use dependency injection. Define constants for window properties to improve clarity.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include "./core/engine.hpp"
+#include "./platform/window.hpp"
+#include "./core/renderer/renderer.hpp"
+#include <iostream>
+
+#define WINDOW_TITLE "CGE"
+#define WIDTH 800
+#define HEIGHT 600
 
 int main() {
-    cge::Engine engine;
-
-    if (!engine.init()) {
+    cge::Window window;
+    if (!window.init(WIDTH, HEIGHT, WINDOW_TITLE)) {
+        std::cerr << "Failed to initialize window" << std::endl;
         return -1;
     }
 
+    cge::Renderer renderer;
+    if (!renderer.init()) {
+        std::cerr << "Failed to initialize renderer" << std::endl;
+        return -1;
+    }
+
+    cge::Engine engine(window, renderer);
+
     engine.run();
+
     return 0;
 }
 
