5f6ac0a6fcd8b7e7e7b4d754c3d4fe4e67015658
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-22 00:02:54 -0300

    feat: implement 'Input' subsystem to 'Engine'

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,21 +1,31 @@
-#include "engine.hpp"
-#include "platform/window.hpp"
-#include "renderer/renderer.hpp"
+
+#include "./engine.hpp"
+#include "./window/window.hpp"
+#include "./renderer/renderer.hpp"
+#include "./input/input.hpp"
+
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
 
 using namespace cge;
 
-#define WIDTH 800
-#define HEIGHT 600
-
-Engine::Engine(Window& window, Renderer& renderer) : mWindow(window), mRenderer(renderer) { }
+Engine::Engine(Window& window, Renderer& renderer, Input& input) 
+    : mWindow(window), mRenderer(renderer), mInput(input) { }
 
 Engine::~Engine() { }
 
 void Engine::run() {
     while (!mWindow.shouldClose()) {
-        mRenderer.clear(0.0f, 0.15f, 0.6f, 1.0f);
+        mRenderer.clear(0.1f, 0.15f, 1.0f, 1.0f);
 
         mRenderer.draw();
+
+        std::string walkForward = "WalkForward";
+        mInput.bindAction(walkForward, GLFW_KEY_W);
+        mInput.bindCallback(walkForward, [&]() {
+            std::cout << "Walking forward\n";
+        });
 
         mWindow.pollEvents();
         mWindow.swapBuffers();
