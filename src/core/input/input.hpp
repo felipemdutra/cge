145e1a91ca4b603fa000e255c5d313a63d20ece2
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-22 17:29:56 -0300

    reafactor: improve Input system Added 'mKeyStates' and 'mPreviousKeyStates' to track when keys are pressed or not Removed static members

--- src/core/input/input.hpp
+++ src/core/input/input.hpp
@@ -16,13 +16,18 @@ namespace cge {
             void bindAction(std::string& action, int key);
             void bindCallback(std::string& action, InputCallback callback);
 
+            void update();
+
         private:
-            GLFWwindow* sWindow = nullptr;
+            GLFWwindow* mWindow = nullptr;
 
             std::unordered_map<int, std::string> mKeyActions;
             std::unordered_map<std::string, InputCallback> mActionCallbacks;
 
+            std::unordered_map<int, bool> mKeyStates;
+            std::unordered_map<int, bool> mPreviousKeyStates;
+
             bool keyDown(int key) const;
     };
-}
+} // namespace cge
 
