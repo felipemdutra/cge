a15624bb978ac42b39e9721d219127f1c5c4a4cb
Felipe Dutra <felipemd.dutra@gmail.com>
2025-02-23 11:14:42 -0300

    feat: add comment documentation Added comments saying what every method does

--- src/core/input/input.hpp
+++ src/core/input/input.hpp
@@ -14,13 +14,43 @@ namespace cge {
 
             bool init(GLFWwindow* window);
 
+            /**
+             * bindKeyAction - Binds a key to an action.
+             *
+             * @param key - The key to bind.
+             * @param action - The action to bind the key to.
+             */
             void bindKeyAction(int key, std::string& action);
+
+            /**
+             * bindMouseButtonAction - Binds a mouse button to an action.
+             *
+             * @param button - The mouse button to bind.
+             * @param action - The action to bind the mouse button to.
+             */
             void bindMouseButtonAction(int button, std::string& action);
 
+            /**
+             * bindCallback - Binds an action to a callback.
+             *
+             * @param action - The action to bind.
+             * @param callback - The callback to bind the action to.
+             */
             void bindCallback(std::string& action, InputCallback callback);
 
+            /**
+             * getMousePosition - Returns the current position of the mouse cursor.
+             */
             glm::vec2 getMousePosition() const;
+
+            /**
+             * getMouseX - Returns the x position of the mouse cursor.
+             */
             float getMouseX() const;
+
+            /**
+             * getMouseY - Returns the y position of the mouse cursor.
+             */
             float getMouseY() const;
 
             void update();
@@ -44,6 +74,13 @@ namespace cge {
             std::unordered_map<int, bool> mPreviousMouseButtonStates;
 
             bool keyDown(int key) const;
+
+            /**
+             * keyDown - Returns true if the key is currently pressed.
+             *
+             * @param key - The key to check.
+             * @return bool - True if the key is currently pressed, false otherwise.
+             */
             bool mouseButtonDown(int button) const;
     };
 } // namespace cge
